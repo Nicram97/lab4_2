e2289267c7e955df8ad3920a9eb80e9e7d035f40
om39385 <marcin952@gmail.com>
2019-03-19 23:18:58 +0100

    Obsługa formularza dodawania

--- app/src/main/java/com/example/lab4_2/MainActivity.java
+++ app/src/main/java/com/example/lab4_2/MainActivity.java
@@ -34,5 +34,16 @@ private ArrayAdapter adapter;
 
     public void nowyWpis(MenuItem mi){
         Intent intencja = new Intent(this, DodajWpis.class);
-        startActivityForResult(intencja, 1);}
+        startActivityForResult(intencja, 1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode==1 && resultCode==RESULT_OK)   {
+            Bundle extras = data.getExtras();
+            String nowy = (String)extras.get("wpis");
+            target.add(nowy);
+            adapter.notifyDataSetChanged();
+        }
+    }
 }
